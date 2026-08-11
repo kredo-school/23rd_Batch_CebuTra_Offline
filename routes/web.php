9b4d8df15305197fb422d874b207f4966b72f6d3
kredoteacherkurt <kredoteacher.kurt@gmail.com>
2026-08-11 16:33:33 +0800

    login fixes

--- routes/web.php
+++ routes/web.php
@@ -86,7 +86,7 @@ Route::post('/logout', [LoginController::class, 'destroy'])->name('logout');
 // Event routes
 // ---------------------------------------------------------------------------
 Route::get('/events', [EventController::class, 'index'])->name('events.index');
-Route::post('/events/store', [EventController::class, 'store'])->name('events.store');
+Route::post('/events/store', [EventCreateController::class, 'store'])->name('events.store');
 Route::get('/events/create/step1', [EventCreateController::class, 'step1'])->name('events.create.step1');
 Route::post('/events/create/step1', [EventCreateController::class, 'storestep1'])->name('storestep1');
 Route::get('/events/create/step2', [EventCreateController::class, 'step2'])->name('events.create.step2');
@@ -133,20 +133,6 @@ Route::middleware(['auth'])->group(function () {
 
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-#イベント一覧ページ
-Route::get('/events',[EventController::class,'index'])->name('events.index');
-Route::post('/events/store',[EventController::class,'store'])->name('events.store');
-#イベント作成ページ
-Route::get('/events/create/step1',[EventCreateController::class,'step1'])->name('events.create.step1');
-Route::post('/events/create/step1',[EventCreateController::class,'storestep1'])->name('storestep1');
-Route::get('/events/create/step2',[EventCreateController::class,'step2'])->name('events.create.step2');
-Route::post('/events/create/step2',[EventCreateController::class,'storestep2'])->name('storestep2');
-Route::get('/events/create/step3',[EventCreateController::class,'step3'])->name('events.create.step3');
-Route::post('/events/create/step3',[EventCreateController::class,'storestep3'])->name('storestep3');
-Route::get('/events/create/step4',[EventCreateController::class,'step4'])->name('events.create.step4');
-Route::post('/events/create/step4',[EventCreateController::class,'storestep4'])->name('storestep4');
-#イベント詳細ページ
-Route::get('/events/{id}',[EventController::class,'show'])->name('events.show');
 #旅程作成ページ
 Route::middleware('auth')->group(function(){
     //旅程
